493539e634610f046ea36f8ac0eeda6b83f3623d
Linda Qin <qinx@college.harvard.edu>
2017-09-30 02:35:00 -0400

    MVP

---
+++ scripts.js
@@ -0,0 +1,71 @@
+window.addEventListener("beforeunload", function () {
+  document.body.classList.add("animate-out");
+});
+
+// $(function () {
+//   $(document).scroll(function () {
+//     var $nav = $(".menu");
+//     $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+//   });
+// });
+
+$(document).ready(function(){
+
+  // Add smooth scrolling to all links
+  $("a").on('click', function(event) {
+
+    // Make sure this.hash has a value before overriding default behavior
+    if (this.hash !== "") {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+
+      // Store hash
+      var hash = this.hash;
+
+      // Using jQuery's animate() method to add smooth page scroll
+      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      $('html, body').animate({
+        scrollTop: $(hash).offset().top
+      }, 800, function(){
+
+        // Add hash (#) to URL when done scrolling (default click behavior)
+        window.location.hash = hash;
+      });
+
+    } // End if
+  });
+
+});
+
+$(function scroll(){
+  $(window).scroll($.throttle(500, function () {
+    console.log(window.scrollY);
+    if (window.scrollY > 60) {
+      $('#backToTop').fadeIn('slow');
+    } else {
+      $('#backToTop').fadeOut('slow');
+    }
+  }));
+})
+
+$(function scrollTop() {
+  $('#backToTop').on('click', function(){
+    $("html, body").animate({ scrollTop: 0 }, 500);
+    return;
+  });
+})
+
+// https://github.com/jlmakes/scrollreveal
+
+window.sr = ScrollReveal({
+  duration: 500,
+  delay: 500,
+  distance: '10px',
+  reset: true,
+  scale: .95,
+});
+sr.reveal('.about-title');
+sr.reveal('.about-2');
+sr.reveal('.about-3');
+sr.reveal('.bar', { origin: 'left', duration: 500 }, 250);
+sr.reveal('.about-4', { delay: 1000, duration: 500 });
